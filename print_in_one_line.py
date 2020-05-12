8c27dcd66a92e98a0625bf40cff0ff9ac796b934
god <tomas.tyser@seznam.cz>
2020-05-12 02:13:31 -0700

    just some simple examples of code to keep

---
+++ print_in_one_line.py
@@ -0,0 +1,4 @@
+books = ['python', 'data science', 'machine learning', 'AI', 'deep learning']
+size = len(books)
+for it in range(0, size):
+    print(books[it], end =", ")
---
+++ print_with_step_param.py
@@ -0,0 +1,4 @@
+#range(start, stop[, step])
+
+for it in range(1, 20, 3):
+    print(it, end = ", ")
