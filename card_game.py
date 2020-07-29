fcb2b8bff31fe57d1fffcbdba45be8839947dd96
Tyserie <tomas.tyser@seznam.cz>
2020-07-29 14:02:23 +0200

    some progress

--- card_game.py
+++ card_game.py
@@ -10,6 +10,32 @@ class Card:
     def __str__(self):
         return f'{Card.rank_names[self.rank]} of {Card.suit_names[self.suit]}'
 
+    def __lt__(self, other):
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return t1 < t2
 
-Queen_of_heart = Card(2, 12)
-print(Queen_of_heart)
+
+class Deck:
+
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+
+deck = Deck()
+print(deck)
+
+# Queen_of_heart = Card(2,12)
+# King_of_Spades = Card(3,13)
+# print(Queen_of_heart)
+# print(King_of_Spades)
