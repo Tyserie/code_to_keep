c7687056f9a531f4307ee7de5757a7e510fe5610
Tyserie <tomas.tyser@seznam.cz>
2020-08-09 17:11:39 +0200

    classes testing

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ class Restaurant():
         """Initialize basic variables"""
         self.name = name
         self.cuisine = cuisine
+        self.number_served = 0
 
     def describe_restaurant(self):
         """Information about restaurant"""
@@ -14,16 +15,24 @@ class Restaurant():
         """Information about availability of restaurant"""
         return print(f"Restaurace {self.name} je otevrena!")
 
+    def set_number_served(self, served):
+        """change number served"""
+        self.number_served = served
+
+    def increment_number_served(self, increment):
+        """Increment number served"""
+        self.number_served += increment
 
 
-moje_restika = Restaurant("Vaclavka", "treti cenova")
-erikovo_restika = Restaurant("Mlsna Koza", "domaci kuchyne")
 conan_restika = Restaurant("U Conana", "grillovane dobroty")
+conan_restika.set_number_served(12)
+conan_restika.increment_number_served(12)
+print(f"{conan_restika.name} served {conan_restika.number_served} customers!")
 
+
+#moje_restika = Restaurant("Vaclavka", "treti cenova")
+#erikovo_restika = Restaurant("Mlsna Koza", "domaci kuchyne")
 #print(f"Hospoda {moje_restika.name} byla {moje_restika.cuisine}, ale dobra.")
-moje_restika.describe_restaurant()
+#moje_restika.describe_restaurant()
 #moje_restika.open_restaurant()
-erikovo_restika.describe_restaurant()
-conan_restika.describe_restaurant()
-
-
+#erikovo_restika.describe_restaurant()
--- test1.py
+++ test1.py
@@ -7,6 +7,7 @@ class User():
         self.surname = surname
         self.username = username
         self.password = password
+        self.login_account = 0
 
     def describe_user(self):
         """User's summary info"""
@@ -16,13 +17,30 @@ class User():
         """Say hello to user"""
         return print(f"Hello {self.name} {self.surname}!")
 
-user1 = User("Tomas", "Tyser", "fbcqlky", "Start123")
-user2 = User("Stefan", "Tomko", "sku15894", "Kapu$tnica")
-user3 = User("Danilo", "Telebakovic", "fkuingyd", "Klepa*89Kk")
+    def increment_login_attempts(self):
+        """Increment login attempts"""
+        self.login_account += 1
 
-user1.describe_user()
-user1.greet_user()
-user2.describe_user()
-user2.greet_user()
-user3.describe_user()
-user3.greet_user()
+    def reset_login_attempts(self):
+        """Reset login attempt"""
+        if self.login_account == 5:
+            self.login_account = 0
+
+user1 = User("Tomas", "Sivak", "yubjklhu", "Start123")
+#user1.describe_user()
+#user1.greet_user()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+print(f"{user1.login_account}")
+user1.reset_login_attempts()
+print(f"{user1.login_account}")
+
+#user2 = User("Stefan", "Tomko", "sku15894", "Kapu$tnica")
+#user3 = User("Danilo", "Telebakovic", "fkuingyd", "Klepa*89Kk")
+#user2.describe_user()
+#user2.greet_user()
+#user3.describe_user()
+#user3.greet_user()
